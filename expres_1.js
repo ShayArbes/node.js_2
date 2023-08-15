2d6772ed60d9ad38533ecb4ef69ebb8e25c3ac02
Shay Arbes <shayarbes1@gmail.com>
2023-08-15 16:59:27 +0300

    3

--- expres_1.js
+++ expres_1.js
@@ -1,13 +1,22 @@
+//-------------------------------------------------------
 const express = require("express");
 // const db = require("./db");
 const app = express();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-let db = fs.readFileSync('./db.json')
+let db = fs.readFileSync("./db.json");
 app.use(express.json());
 db = JSON.parse(`${db}`);
-console.log(db);
-let s = {email: "shayarbes@gmail.com",password: "123"};
+//-------------------------------------------------------
+let s = { email: "shayarbes@gmail.com", password: "123" };
+//-------------------------------------------------------
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
+
+//-------------------------------------------------------
+
 app.get("/users", (req, res) => {
   res.send(JSON.stringify(db));
 });
@@ -30,13 +39,71 @@ app.post("/users", (req, res) => {
 
     return res.status(400).json({ error: "Missing required fields" });
   }
-  const newUser = {id:uuidv4(), email, password };
+  const newUser = { id: uuidv4(), email, password };
   db.push(newUser);
 
-  fs.writeFile("db.json", JSON.stringify(db), (err) => {
-    if (err) throw err;
-    console.log("The file has been saved!");
-  });
+  saveToFile(db);
 });
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    console.log(email);
+    console.log(password);
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+  const user = db.find((u) => {
+    return u.email === email && u.password === password;
+  });
+  if (user) {
+    res.send(`Hello ${email}`);
+  } else {
+    res.send("User does not exist!");
+  }
+});
+
+app.put("/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const index = db.findIndex((user) => user.id === userId);
+  console.log(userId);
+
+  console.log(index);
+  if (index === undefined) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  const { email, password } = req.body;
+  console.log(email);
+  console.log(password);
+  db[index].email = email ? email : db.email;
+  db[index].password = password ? password : db.password;
+
+  saveToFile(db);
+
+  return res.status(200).send();
+});
+
+
+app.delete("/users/:id", (req, res) => {
+    const userId = parseInt(req.params.id);
+    const index = db.findIndex((user) => user.id === userId);
+    console.log(userId);
+  
+    console.log(index);
+    if (index === undefined) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const { email, password } = req.body;
+    db.splice(index,1);
+  
+    saveToFile(db);
+  
+    return res.status(200).send();
+  });
+
+const saveToFile = (db)=>{
+    fs.writeFile("db.json", JSON.stringify(db), (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+      });
+}
 app.listen(3000, () => console.log("Server started on port 3000"));
